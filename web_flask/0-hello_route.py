3d354aa29825c538bb75bc98083591d7a1119e89
fardosa001 <fardosaadow@gmail.com>
2023-12-30 09:34:16 +0000

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
-listening on 0.0.0.0, port 5000
+	starts a Flask web application
+	listening on 0.0.0.0, port 5000
 """
-
 
 from flask import Flask
 app = Flask(__name__)
@@ -16,4 +15,4 @@ def hello_hbnb():
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, debug=True)
+	app.run(host="0.0.0.0", port=5000, debug=True)
