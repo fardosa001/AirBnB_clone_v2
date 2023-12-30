fc5327ded9acd024a7e4c8143d3e46947dbacf3d
fardosa001 <fardosaadow@gmail.com>
2023-12-30 12:24:01 +0000

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,5 +17,11 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """display C"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
