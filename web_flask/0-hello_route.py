41971456b5e1b250c011b57f1cccb186b8ee8ea6
fardosa001 <fardosaadow@gmail.com>
2023-12-30 03:39:32 -0800

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	"""display Hello HBNB!"""
-	return "Hello HBNB!"
+    """display Hello HBNB!"""
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,16 +4,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	"""display text"""
-	return "Hello HBNB!"
+    """display text"""
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""display text"""
-	return 'HBNB'
+    """display text"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
