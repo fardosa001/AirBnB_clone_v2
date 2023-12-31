c000124ff22fba82834a165a549cf396a1ed1703
fardosa001 <fardosaadow@gmail.com>
2023-12-31 09:47:42 +0000

    Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,7 +49,7 @@ def html_if_odd_even(n):
     """display a HTML page only if n is an integer"""
     odd_or_even = "even" if (n % 2 == 0) else "odd"
     return render_template('6-number_odd_or_even.html',
-                            n=n, odd_or_even=odd_or_even)
+                           n=n, odd_or_even=odd_or_even)
 
 
 if __name__ == '__main__':
