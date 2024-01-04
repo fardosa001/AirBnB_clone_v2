8fffede940301d67e2da33f96684e5df7ded0a05
fardosa001 <fardosaadow@gmail.com>
2024-01-04 03:56:19 -0800

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,1 +1,80 @@
-#!
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from sqlalchemy.orm import scoped_session
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display text"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """display C"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text="is cool"):
+    """display “Python ”, followed by the value of the text
+    variable (replace underscore _ symbols with a space )
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_integer(n):
+    """display “n is a number” only if n is an integer"""
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def html_if_odd_even(n):
+    """display a HTML page only if n is an integer"""
+    odd_or_even = "even" if (n % 2 == 0) else "odd"
+    return render_template('6-number_odd_or_even.html',
+                           n=n, odd_or_even=odd_or_even)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """display a HTML page"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """after each request remove current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display html webpage"""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
